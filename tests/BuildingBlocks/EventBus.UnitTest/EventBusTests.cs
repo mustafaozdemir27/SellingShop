7ca7a29de464c99d1f97eb26034f04f2b4bac4d0
Mustafa Özdemir <by.ozdemr@gmail.com>
2025-04-14 16:58:41 +0300

    EventBus UnitTest Added

---
+++ tests/BuildingBlocks/EventBus.UnitTest/EventBusTests.cs
@@ -0,0 +1,126 @@
+﻿using EventBus.UnitTest.Events.EventHandlers;
+using EventBus.UnitTest.Events.Events;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using EventBus.Factory;
+using EventBus.Base.Abstraction;
+using EventBus.Base;
+
+namespace EventBus.UnitTest
+{
+    [TestClass]
+    public sealed class EventBusTests
+    {
+        private ServiceCollection services;
+
+        public EventBusTests()
+        {
+            this.services = new ServiceCollection();
+            services.AddLogging(configure => configure.AddConsole());
+        }
+
+        [TestMethod]
+        public void subscribe_event_on_rabbitmq_test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                return EventBusFactory.Create(GetRabbitMQConfig(), sp);
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+            var eventBus = serviceProvider.GetRequiredService<IEventBus>();
+
+            eventBus.Subscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+
+            //eventBus.UnSubscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+        }
+
+        //[TestMethod]
+        //public void subscribe_event_on_azure_test()
+        //{
+        //    services.AddSingleton<IEventBus>(sp =>
+        //    {
+        //        return EventBusFactory.Create(GetAzureConfig(), sp);
+        //    });
+
+        //    var serviceProvider = services.BuildServiceProvider();
+        //    var eventBus = serviceProvider.GetRequiredService<IEventBus>();
+
+        //    eventBus.Subscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+
+        ////    eventBus.UnSubscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+        
+        // Task.Delay(2000).Wait();
+        //}
+
+        [TestMethod]
+        public void send_message_to_rabbitmq_test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                return EventBusFactory.Create(GetRabbitMQConfig(), sp);
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var eventBus = serviceProvider.GetRequiredService<IEventBus>();
+
+            eventBus.Publish(new OrderCreatedIntegrationEvent(1));
+        }
+
+        //[TestMethod]
+        //public void send_message_to_azure_test()
+        //{
+        //    services.AddSingleton<IEventBus>(sp =>
+        //    {
+        //        return EventBusFactory.Create(GetAzureConfig(), sp);
+        //    });
+
+        //    var serviceProvider = services.BuildServiceProvider();
+
+        //    var eventBus = serviceProvider.GetRequiredService<IEventBus>();
+
+        //    eventBus.Publish(new OrderCreatedIntegrationEvent(1));
+        //}
+
+        [TestMethod]
+        public void consume_ordercreated_from_rabbitmq_test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                return EventBusFactory.Create(GetRabbitMQConfig(), sp);
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var eventBus = serviceProvider.GetRequiredService<IEventBus>();
+
+
+        }
+
+        private EventBusConfig GetAzureConfig()
+        {
+            return new EventBusConfig()
+            {
+                ConnectionRetryCount = 5,
+                SubscriberClientAppName = "EventBus.UnitTest",
+                DefaultTopicName = "UnitTestTopicName",
+                EventBusType = EventBusType.AzureServiceBus,
+                EventNamePrefix = "IntegrationEvent",
+                EventBusConnectionString = "***"
+            };
+        }
+
+        private EventBusConfig GetRabbitMQConfig()
+        {
+            return new EventBusConfig()
+            {
+                ConnectionRetryCount = 5,
+                SubscriberClientAppName = "EventBus.UnitTest",
+                DefaultTopicName = "UnitTestTopicName",
+                EventBusType = EventBusType.RabbitMQ,
+                EventNamePrefix = "IntegrationEvent",
+            };
+        }
+    }
+}
---
+++ tests/BuildingBlocks/EventBus.UnitTest/Events/EventHandlers/OrderCreatedIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using EventBus.Base.Abstraction;
+using EventBus.UnitTest.Events.Events;
+
+namespace EventBus.UnitTest.Events.EventHandlers
+{
+    public class OrderCreatedIntegrationEventHandler : IIntegrationEventHandler<OrderCreatedIntegrationEvent>
+    {
+        public Task Handle(OrderCreatedIntegrationEvent @event)
+        {
+            Console.WriteLine("Handle method worked with id:" + @event.Id);
+            return Task.CompletedTask;
+        }
+    }
+}

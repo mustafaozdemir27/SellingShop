ac7607c1bc99533cfdd6d5bbe9ded44308da654f
Mustafa Özdemir <by.ozdemr@gmail.com>
2025-04-22 11:10:18 +0300

    Order Service - Domain Layer updated

---
+++ src/Services/OrderService/OrderService.Domain/AggregateModels/BuyerAggregate/CardType.cs
@@ -0,0 +1,17 @@
+﻿using OrderService.Domain.SeedWork;
+
+namespace OrderService.Domain.AggregateModels.BuyerAggregate
+{
+    public class CardType : Enumeration
+    {
+        public static CardType AmericanExpress = new(1, nameof(AmericanExpress));
+        public static CardType Visa = new(2, nameof(Visa));
+        public static CardType MasterCard = new(3, nameof(MasterCard));
+        public static CardType Troy = new(4, nameof(Troy));
+
+        public CardType(int id, string name) : base(id, name)
+        {
+
+        }
+    }
+}

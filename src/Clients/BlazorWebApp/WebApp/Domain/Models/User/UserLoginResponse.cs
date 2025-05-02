8d014765c3f66c05fd2db3ad3741c373e9787d48
Mustafa Özdemir <by.ozdemr@gmail.com>
2025-05-02 13:45:39 +0300

    deleted blazor

--- src/Clients/BlazorWebApp/WebApp/Domain/Models/User/UserLoginResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApp.Domain.Models.User
-{
-    public class UserLoginResponse
-    {
-        public string UserName { get; set; }
-        public string UserToken { get; set; }
-    }
-}
